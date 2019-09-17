9c19d183c6743cb547cb1d01926e12fa122915e8
Codetector <codetector@codetector.cn>
2019-09-17 12:43:41 -0400

    VAlign Center is ImPoSsIbLe

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,16 +2,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes';
-import styles from './Home.css';
+import styles from './Home.scss';
 
 type Props = {};
 
 export default class Home extends Component<Props> {
   props: Props;
 
-  render() {
+  render () {
     return (
-      <div className={styles.container} data-tid="container">
+      <div className={styles.container}>
         <h2>Home</h2>
         <Link to={routes.COUNTER}>to Counter</Link>
       </div>

51616fa1d1ff4257d86ae38c4529162c1ea7e444
codetector <codetector@codetector.cn>
2019-09-18 09:55:04 -0400

    Inventory Search

--- app/components/Inventory.js
+++ app/components/Inventory.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-import { Col, Row } from 'antd';
+import { Button, Col, Row, Input } from 'antd';
 import { Link } from 'react-router-dom';
 import styles from './inventory.scss';
 import routes from '../constants/routes';
+
+const { Search } = Input;
 
 export default class Inventory extends React.Component {
   render() {
@@ -14,8 +16,20 @@ export default class Inventory extends React.Component {
             <h2>Hive Inventory Lookup</h2>
           </Col>
         </Row>
-        <Row type="flex" justify="center">
-          Filler
+        <Row type="flex" justify="center" className={styles.inventorySearchBar}>
+          <Col span={10}>
+            <Search
+              placeholder="Input name, type, id, or label to search"
+              enterButton="Search"
+              size="large"
+              onSearch={value => console.log(value)}
+            />
+          </Col>
+          <Col span={4}>
+            <Button type="primary" icon="plus" size="large">
+              Add Item
+            </Button>
+          </Col>
         </Row>
         <div className={styles.backContainer}>
           <Link to={routes.HOME}>
